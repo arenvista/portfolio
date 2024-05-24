14c42d80e3bd4eb1d8f0934720ff115e56aafcb6
sybil <arenvista1@gmail.com>
2024-05-24 14:38:45 -0400

    skills section and slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/navbar.js';
+import { Skills } from './components/skills.js';
+import { Banner } from './components/banner.js';
+import  'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
             <NavBar />
+            <Banner />
+            <Skills />
     </div>
   );
 }
