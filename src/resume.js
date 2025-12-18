ef0e146b47b786b3c0a9abc52cf54187ea057e00
sybil-thinker <arenvista1@gmail.com>
2025-12-18 16:03:43 -0500

    formated cv

--- src/resume.js
+++ src/resume.js
@@ -5,7 +5,6 @@ import './resume-styles.css'; // Make sure this file exists in the same folder
 export const Resume = () => {
   return (
     <div id="content" className="content">
-      {/* Header */}
       <div className="header" style={{ textAlign: 'center' }}>
         <h1>Aren Vista - Curriculum Vitae</h1>
         <div>
@@ -13,7 +12,6 @@ export const Resume = () => {
         </div>
       </div>
 
-      {/* Education */}
       <div id="education" className="outline-2">
         <h2>Education</h2>
         <div className="outline-text-2">
@@ -44,24 +42,21 @@ export const Resume = () => {
         </div>
       </div>
 
-      {/* Experience */}
       <div id="experience" className="outline-2">
         <h2>Experience</h2>
         <div className="outline-text-2">
-          
+
           <table>
             <tbody>
               <tr>
                 <td>Undergraduate Researcher: Mathematics</td>
                 <td className="right-align">December 2025 - Present</td>
               </tr>
-              <tr>
-                <td className="left-algin">Bedrich Sousedik: University of Maryland, Baltimore County</td>
-              </tr>
+              <tr><td className="left-align">Bedrich Sousedik: University of Maryland, Baltimore County</td></tr>
               <tr>
                 <td colSpan="3">
                   <ul>
-                    <li>_</li>
+                    <li>Research on optimized betting strategies using MCMC</li>
                   </ul>
                 </td>
               </tr>
@@ -74,14 +69,12 @@ export const Resume = () => {
                 <td>Undergraduate Researcher: Computer Engineering</td>
                 <td className="right-align">December 2025 - Present</td>
               </tr>
-              <tr>
-                <td className="left-algin">Riadul Islam: University of Maryland, Baltimore County</td>
-              </tr>
+              <tr><td className="left-align">Riadul Islam: University of Maryland, Baltimore County</td></tr>
               <tr>
                 <td colSpan="3">
                   <ul>
-                    <li>Developed Spiking Neural Network for the classification of temporal data TBD</li>
-                    <li>Developed CNN Networks for image classification TBD</li>
+                    <li>Developing Spiking Neural Networks (SNNs) for the classification of temporal data.</li>
+                    <li>Designing Convolutional Neural Networks (CNNs) for image classification tasks.</li>
                   </ul>
                 </td>
               </tr>
@@ -94,15 +87,13 @@ export const Resume = () => {
                 <td>Project Lead - Computational Biologist</td>
                 <td className="right-align">June 2021 - June 2023</td>
               </tr>
-              <tr>
-                <td className="left-algin">National Institute of Health</td>
-              </tr>
+              <tr><td className="left-align">National Institute of Health</td></tr>
               <tr>
                 <td colSpan="3">
                   <ul>
-                    <li>Established and managed a software version control system using Git to streamline code branch management and release control, while coordinating development efforts across multiple laboratories.</li>
-                    <li>Deployed unsupervised machine learning algorithms, specifically K-Means and DBSCAN, to cluster multivariate data for anomaly detection, resulting in the identification of key contributors to disease pathology.</li>
-                    <li>Engineered an automated data processing pipeline utilizing SWARM and High-Performance Computing (HPC) environments to generate 3D amino acid structures from linear sequences. This optimization enhanced vector design, increasing packageable information in viral vectors.</li>
+                    <li>Established and managed a software version control system using Git to streamline release control and coordinate development efforts across multiple laboratories.</li>
+                    <li>Deployed unsupervised machine learning algorithms, including K-Means and DBSCAN, to cluster multivariate data for anomaly detection, identifying key contributors to disease pathology.</li>
+                    <li>Engineered an automated data processing pipeline utilizing SWARM and High-Performance Computing (HPC) environments to generate 3D amino acid structures, optimizing viral vector payload capacity.</li>
                     <li>Developed a command-line interface (CLI) tool for sequence alignment and custom scoring of novel sequences, facilitating similarity analysis across closely related datasets.</li>
                     <li>Mentored summer interns and staff scientists in Python programming, Git version control protocols, and Bash scripting.</li>
                   </ul>
@@ -118,16 +109,16 @@ export const Resume = () => {
                 <td className="right-align">June 2017 - June 2020</td>
               </tr>
               <tr>
-                <td className="left-algin">Seley Radtke Lab: University of Maryland, Baltimore County</td>
+                <td className="left-align">Seley Radtke Lab: University of Maryland, Baltimore County</td>
               </tr>
               <tr>
                 <td colSpan="3">
                   <ul>
-                    <li>Leveraged Python, BeautifulSoup (BS4), and Selenium to aggregate extensive drug databases for the documentation of approved nucleoside analogues.</li>
-                    <li>Devised a synthetic pathway for preparing N-glycosylated imidazole 4-boronic acids, which improved reaction yields by 40%.</li>
+                    <li>Leveraged Python, BeautifulSoup (BS4), and Selenium to curate a comprehensive database of approved nucleoside analogues.</li>
+                    <li>Devised a synthetic pathway for preparing N-glycosylated imidazole 4-boronic acids, improving reaction yields by 40%.</li>
                     <li>Engineered nucleoside analog constructs to produce novel single-chain linked nucleoside heterocycles, designated as fleximers.</li>
                     <li>Synthesized precursor molecules to support the creation of flex-Remdesivir analogues.</li>
-                    <li>Synthesized flexible nucleoside analogues to combat infectious diseases.</li>
+                    <li>Synthesized flexible nucleoside analogues designed to combat infectious diseases.</li>
                     <li>Targeted viral replication complexes in SARS-CoV and Ebola.</li>
                     <li>Utilized Suzuki coupling to synthesize 7-deaza fleximer bases.</li>
                     <li>Achieved consistent high yields across a 4-step synthetic pathway.</li>
@@ -144,18 +135,16 @@ export const Resume = () => {
                 <td>Undergraduate Researcher - Molecular Biology</td>
                 <td className="right-align">June 2017 - June 2020</td>
               </tr>
-              <tr>
-                <td className="left-align">Vasta Lab - IMET</td>
-              </tr>
+              <tr><td className="left-align">Vasta Lab - IMET</td></tr>
               <tr>
                 <td colSpan="3">
                   <ul>
                     <li>Formulated a synthesis scheme for lactose-conjugated Sepharose, achieving a fivefold reduction in reagent costs.</li>
                     <li>Designed and optimized primers for cloning experiments, ensuring successful gene amplification.</li>
-                    <li>Directed the standardization and optimization of protocols regarding protein purification, characterization, and modification.</li>
+                    <li>Directed the standardization and optimization of protein purification, characterization, and modification protocols.</li>
                     <li>Trained interns on standard laboratory operating procedures and scientific writing methodologies.</li>
-                    <li>Calibrated analytical equipment and constructed calibration curves to improve accuracy on non-standard samples</li>
-                    <li>Categorized the carbohydrate recognition of ligands on Perkinsus Parasites and their impact on the Chesapeake</li>
+                    <li>Calibrated analytical equipment and constructed calibration curves to improve accuracy on non-standard samples.</li>
+                    <li>Categorized carbohydrate recognition ligands on Perkinsus parasites to assess their ecological impact on the Chesapeake Bay.</li>
                   </ul>
                 </td>
               </tr>
@@ -164,7 +153,6 @@ export const Resume = () => {
         </div>
       </div>
 
-      {/* Conferences */}
       <div id="conferences" className="outline-3">
         <h2>Conference Contributions</h2>
         <div className="outline-text-3">
@@ -195,7 +183,6 @@ export const Resume = () => {
         </div>
       </div>
 
-      {/* Publications */}
       <div id="publications" className="outline-3">
         <h2>Publications</h2>
         <div className="outline-text-3">
@@ -203,7 +190,6 @@ export const Resume = () => {
         </div>
       </div>
 
-      {/* Acknowledgements */}
       <div id="Acknowledgements" className="outline-3">
         <h2>Acknowledgements</h2>
         <div className="outline-text-3">
@@ -211,7 +197,6 @@ export const Resume = () => {
         </div>
       </div>
 
-      {/* Teaching */}
       <div id="teaching" className="outline-3">
         <h2>Teaching</h2>
         <table>
@@ -220,9 +205,7 @@ export const Resume = () => {
               <td>CAPSTONE Coordinator</td>
               <td className="right-align">January 2025 - August 2025</td>
             </tr>
-            <tr>
-              <td className="left-algin">Stephen Freeland, Interm Vice-Provost: University of Maryland, Baltimore County</td>
-            </tr>
+            <tr><td className="left-align">Stephen Freeland, Interim Vice-Provost: University of Maryland, Baltimore County</td></tr>
           </tbody>
         </table>
 
@@ -232,9 +215,7 @@ export const Resume = () => {
               <td>Teaching Assistant: Linear Algebra</td>
               <td className="right-align">August 2025 - Present</td>
             </tr>
-            <tr>
-              <td className="left-algin">Osman Guler, Daniel Reynolds: University of Maryland, Baltimore County</td>
-            </tr>
+            <tr><td className="left-align">Osman Guler, Daniel Reynolds: University of Maryland, Baltimore County</td></tr>
           </tbody>
         </table>
 
@@ -244,27 +225,66 @@ export const Resume = () => {
               <td>CRLA Certified Tutor</td>
               <td className="right-align">August 2025 - Present</td>
             </tr>
-            <tr>
-              <td className="left-algin">Academic Sucess Center: University of Maryland, Baltimore County</td>
-            </tr>
+            <tr><td className="left-align">Academic Success Center: University of Maryland, Baltimore County</td></tr>
             <tr>
               <td colSpan="3">
-                <ul>MATH 151 <b>Calculus and Analytic Geometry I</b></ul>
-                <ul>MATH 152 <b>Calculus and Analytic Geometry II</b></ul>
-                <ul>MATH 251 <b>Multivariate Calculus</b></ul>
-                <ul>MATH 221 <b>Linear Algebra</b></ul>
-                <ul>MATH 225 <b>Differential Equations</b></ul>
-                <ul>MATH 300 <b>Mathematical Reasoning</b></ul>
-                <ul>CMSC 201 <b>Computer Science I</b></ul>
-                <ul>CMSC 202 <b>Computer Science II</b></ul>
-                <ul>CMSC 341 <b>Data Structures</b></ul>
-                <ul>CMSC 203 <b>Discrete Structures</b></ul>
-                <ul>BIO 141 <b>Foundations of Biology</b></ul>
-                <ul>PHYS 121 <b>Introductory Physics</b></ul>
+                <ul>
+                  MATH 151
+                  <b> Calculus and Analytic Geometry I</b>
+                </ul>
+                <ul>
+                  MATH 152
+                  <b> Calculus and Analytic Geometry II</b>
+                </ul>
+                <ul>
+                  MATH 251
+                  <b> Multivariate Calculus</b>
+                </ul>
+
+                <ul>
+                  MATH 221
+                  <b> Linear Algebra</b>
+                </ul>
+
+                <ul>
+                  MATH 225
+                  <b> Differential Equations</b>
+                </ul>
+
+                <ul>
+                  MATH 300
+                  <b> Mathematical Reasoning</b>
+                </ul>
+
+                <ul>
+                  CMSC 201
+                  <b> Computer Science I</b>
+                </ul>
+                <ul>
+                  CMSC 202
+                  <b> Computer Science II</b>
+                </ul>
+                <ul>
+                  CMSC 341
+                  <b> Data Structures</b>
+                </ul>
+                <ul>
+                  CMSC 203
+                  <b> Discrete Structures</b>
+                </ul>
+                <ul>
+                  BIO 141
+                  <b> Foundations of Biology</b>
+                </ul>
+                <ul>
+                  PHYS 121
+                  <b> Introductory Physics</b>
+                </ul>
               </td>
             </tr>
           </tbody>
         </table>
+
 
         <table>
           <tbody>
@@ -272,24 +292,38 @@ export const Resume = () => {
               <td>Freelance Tutor</td>
               <td className="right-align">January 2025 - Present</td>
             </tr>
-            <tr>
-              <td className="left-algin">Varsity Tutors</td>
-            </tr>
+            <tr><td className="left-align">Varsity Tutors</td></tr>
             <tr>
               <td colSpan="3">
-                <ul>All Prior Listed Courses</ul>
-                <ul>CHEM 101 <b>Principles of Chemistry I</b></ul>
-                <ul>CHEM 102 <b>Principles of Chemistry II</b></ul>
-                <ul>CHEM 300 <b>Analytical Chemistry</b></ul>
-                <ul>CHEM 351 <b>Organic Chemistry I</b></ul>
-                <ul>CHEM 352 <b>Organic Chemistry II</b></ul>
+                <ul>
+                  Tutored all previously listed courses including:
+                </ul>
+                <ul>
+                  CHEM 101
+                  <b> Principles of Chemistry I</b>
+                </ul>
+                <ul>
+                  CHEM 102
+                  <b> Principles of Chemistry II</b>
+                </ul>
+                <ul>
+                  CHEM 300
+                  <b> Analytical Chemistry</b>
+                </ul>
+                <ul>
+                  CHEM 351 
+                  <b> Organic Chemistry I</b>
+                </ul>
+                <ul>
+                  CHEM 352
+                  <b> Organic Chemistry II</b>
+                </ul>
               </td>
             </tr>
           </tbody>
         </table>
       </div>
 
-      {/* Skills */}
       <div id="skills" className="outline-2">
         <h2>Skills</h2>
         <div className="outline-text-2">
@@ -302,7 +336,6 @@ export const Resume = () => {
         </div>
       </div>
 
-      {/* Projects */}
       <div id="projects" className="outline-2">
         <h2>Projects</h2>
         <div className="outline-text-2">
@@ -314,6 +347,7 @@ export const Resume = () => {
           </ul>
         </div>
       </div>
+
     </div>
   );
 };
